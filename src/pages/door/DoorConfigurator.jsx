ff8379861d46786a60a27b054c10db5c2d54f209
DDstar1 <abhuluimendestiny@gmail.com>
2026-02-11 19:23:03 +0100

    dd

--- src/pages/door/DoorConfigurator.jsx
+++ src/pages/door/DoorConfigurator.jsx
@@ -10,79 +10,44 @@ import {
   useTexture,
 } from "@react-three/drei";
 import { GizmoHelper, Environment } from "@react-three/drei";
-import { useControls } from "leva";
-import { SpotLightHelper } from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
 import { DoorModelJSX } from "@/pages/door/3d_components/Door";
-
-function LightWithHelper() {
-  const { intensity, color } = useControls({
-    intensity: { value: 0.8, min: 0, max: 5, step: 0.1 },
-    color: "#ffffff",
-  });
-
-  return <ambientLight intensity={0.8} />;
-}
-
-function AnimatedBox() {
-  const boxRef = useRef();
-
-  const { color, speed } = useControls({
-    color: "#0000ff",
-    speed: { value: 0.005, min: 0.001, max: 0.08, step: 0.001 },
-  });
-
-  const [wireFrame, setWireFrame] = useState(false);
-
-  const handleClick = () => {
-    setWireFrame(!wireFrame);
-    console.log("Box clicked! Wireframe mode:", !wireFrame);
-  };
-
-  useFrame(() => {
-    boxRef.current.rotation.x += speed;
-    boxRef.current.rotation.y += speed;
-    boxRef.current.rotation.z += speed;
-  });
-  return (
-    <mesh castShadow ref={boxRef} onClick={handleClick} position={[3, 3, 3]}>
-      <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color={color} wireframe={wireFrame} />
-    </mesh>
-  );
-}
-
-function Model() {
-  const result = useGLTF("/models/door.glb");
-  console.log(result);
-  return <primitive object={result.scene} />;
-}
-
-function SphereWithTexture() {
-  const texture = useTexture("/textures/wood/wood_texture.jpg");
-  return (
-    <mesh position={[-2, 3, 2]}>
-      <sphereGeometry args={[0.5, 32, 32]} />
-      <meshStandardMaterial map={texture} />
-    </mesh>
-  );
-}
+import DoorConfiguratorForm from "@/components/door_form";
 
 export default function DoorConfigurator() {
+  const [glassType, setGlassType] = useState("Klar");
+  const [handle, setHandle] = useState("Ohne");
+  const [handleColor, setHandleColor] = useState("Matt Schwarz");
   return (
-    <div id="door_canvas" className="h-screen w-screen">
+    <div className="h-screen w-screen relative">
+      {/* Configurator Form Overlay */}
+      <DoorConfiguratorForm
+        glassType={glassType}
+        setGlassType={setGlassType}
+        handle={handle}
+        setHandle={setHandle}
+        handleColor={handleColor}
+        setHandleColor={setHandleColor}
+      />
+
+      {/* 3D Canvas */}
       <Canvas shadows camera={{ position: [2, 1, 2] }}>
         <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
           <GizmoViewport />
         </GizmoHelper>
         <axesHelper args={[10]} />
         <gridHelper args={[40, 20, "red", 0x55ccff]} />
-        <OrbitControls enablePan={true} />
-        <directionalLight intensity={2} position={[2, 5, 1]} />;
+        <OrbitControls target={[1.2, 2.1, -0.971]} enablePan={true} />
+        <directionalLight intensity={2} position={[2, 5, 1]} />
         <Environment preset="city" />
-        <DoorModelJSX position={[0, 0, 0]} />
-        {/* <AnimatedBox /> */}
-        {/*   <SphereWithTexture /> */}
+
+        {/* Door Model with props */}
+        <DoorModelJSX
+          position={[0, 0, 0]}
+          glassType={glassType}
+          handle={handle}
+          handleColor={handleColor}
+        />
       </Canvas>
     </div>
   );

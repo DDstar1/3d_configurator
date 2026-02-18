8d2268b78f82deadf0db0a653b346963af1a09dc
DDstar1 <abhuluimendestiny@gmail.com>
2026-02-18 20:36:01 +0100

    dd

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-import "./tailwind_global.css";
+
+import "./tailwind_index.css";
 import { BrowserRouter } from "react-router-dom";
 import { CustomizationProvider } from "@/contexts/Customization";
 
